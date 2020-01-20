cf101c8ce4e17596623b9bc7b37af209fc1652d0
MatthewPantaleon <45208898+MatthewPantaleon@users.noreply.github.com>
2020-01-20 09:55:22 +0000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+/**
+ * @Date:   2020-01-20T09:50:32+00:00
+ * @Last modified time: 2020-01-20T09:53:00+00:00
+ */
+
+
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
