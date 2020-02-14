2d3915cc356d92ee5ff65c0da6a99cefef7ac28d
MatthewPantaleon <45208898+MatthewPantaleon@users.noreply.github.com>
2020-02-14 13:54:42 +0000

    Update FormPanel.js

--- src/components/FormPanel.js
+++ src/components/FormPanel.js
@@ -1,6 +1,6 @@
 /**
  * @Date:   2020-02-11T18:30:44+00:00
- * @Last modified time: 2020-02-13T13:13:37+00:00
+ * @Last modified time: 2020-02-14T13:54:10+00:00
  */
 
  import React, { Component, Fragment } from 'react';
@@ -21,8 +21,13 @@
 
    async componentDidMount(){
      // console.log(ApiLoader("games", false));
-     let companyDetail = await ApiLoader("company/" + localStorage.getItem("company_id")).then((d) => d.data).catch((d) => {return {data: [], name: ""}});
-     this.setState({company: companyDetail}, () => console.log(this.state.company));
+     let companyDetail = await ApiLoader("company/" + localStorage.getItem("company_id"), "get").then((d) => d.data).catch((d) => {return {data: [], name: ""}});
+     this.setState({company: companyDetail});
+   }
+
+   submitGame(e){
+     e.preventDefault();
+     console.log("NEW GAME");
    }
 
 
@@ -35,8 +40,27 @@
             <small>{this.state.company.name}</small>
           </div>
           <hr className="m-0 p-0" style={{border: "2px solid black"}}/>
-          <div className="card-body bg-dark">
-
+          <div className="card-body bg-dark text-white">
+          <form onSubmit={(e) => this.submitGame(e)}>
+            <div className="form-group row">
+              <div className="col-3"><label className="col-form-label" htmlFor="name">Name: </label></div>
+              <div className="col-9"><input name="name" className="form-control" autoComplete="off"/></div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="gameDesc">Game Description:</label>
+              <textarea className="form-control" name="gameDesc" rows="10"></textarea>
+            </div>
+            <div className="form-group row">
+              <div className="col-3"><label className="col-form-label" htmlFor="price">Price: </label></div>
+              <div className="input-group mb-2 col-9">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">€</div>
+                </div>
+                <input type="text" className="form-control" name="price"/>
+              </div>
+            </div>
+            <button className="btn btn-primary">Add New Game</button>
+          </form>
           </div>
         </div>
        </>
